fbf173de9abc9eabca2d074d9c878ce88129406d
dsilan <dorsinsilan.isik@ogr.iu.edu.tr>
2021-01-27 12:13:46 +0300

    updated

--- WordCount.scala
+++ WordCount.scala
@@ -9,9 +9,21 @@ object WordCount {
     val sc = new SparkContext("local[*]", "WordCount")
 
     val data = sc.textFile("data/book.txt")
-    val words = data.flatMap(x => x.split(' '))
-    //counts the occurrences of each word
-    val countByWord = words.countByValue()
-    countByWord.foreach(println)
+    val words = data.flatMap(x => x.split("\\W+"))
+    val lowerCase = words.map(x=> x.toLowerCase())
+    //counts the occurrences of each word . First way:
+    //val countByWord = lowerCase.countByValue()
+    //countByWord.foreach(println)
+
+    val wordCounts = lowerCase.map(x=>(x,1)).reduceByKey((x,y) => x+y)
+
+    //To be able to sort by key we need to swap places
+    val wordCountsSorted=wordCounts.map(x=> (x._2,x._1)).sortByKey()
+    for(results<-wordCountsSorted){
+      val word = results._2
+      val count= results._1
+
+      println(s"$word:$count")
+    }
   }
 }
