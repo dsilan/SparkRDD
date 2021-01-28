e6964fabad795b624762f6053742643c82345384
dsilan <dorsinsilan.isik@ogr.iu.edu.tr>
2021-01-28 10:43:53 +0300

    updated

--- CustomerOrders.scala
+++ CustomerOrders.scala
@@ -3,18 +3,15 @@ import org.apache.spark.SparkContext
 
 object CustomerOrders {
 
-  def parseLine(line: String):(String,Float)={
-    val fields = line.split(',')
-    val age = fields(0)
-    val amount = fields(2).toFloat
-
-    (age, amount)
+  def parseLine(line: String): (Int, Float) = {
+    val fields = line.split(",")
+    (fields(0).toInt, fields(2).toFloat)
   }
 
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val sc = new SparkContext("local[*]", "WordCount")
+    val sc = new SparkContext("local[*]", "CustomerOrders")
 
     val rdd = sc.textFile("data/customer-orders.csv")
     val tuples = rdd.map(parseLine) //key,value => customer,amount
